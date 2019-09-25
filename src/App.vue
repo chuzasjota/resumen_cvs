ebe393032772a9b6b8222ed9d900164d67d27bad
chuzasjota <chuzas.j@gmail.com>
2019-09-25 08:21:00 -0500

    Creacion de componente Form - Visualizar Form segun archivo json schema

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Form
   }
 }
 </script>
+<style lang="scss">
+  // Import Bulma's core
+  @import "~bulma/sass/utilities/_all";
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  // Import Bulma and Buefy styles
+  @import "~bulma";
+  @import "~buefy/src/scss/buefy";
 </style>
---
+++ src/components/Form.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="section">
+    <h1>Resume Schem Form</h1>
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <schema-form :schema="schema" v-model="formData" @submit="submit()" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import {SchemaForm} from 'vue-json-schema-form'
+import schema from '../schemas/schema.json'
+
+export default {
+  name: "Form",
+  components: {
+    SchemaForm
+  },
+  data() {
+    return {
+      schema: schema,
+      formData: {}
+    };
+  },
+  methods: {
+    submit() {
+      console.log(JSON.stringify(this.formData, null, 2))
+      // do things with the validated submission (this.formData)
+    }
+  }
+}
+</script>
