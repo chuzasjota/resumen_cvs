6991779070900c454f80560a318b294105ab9f53
chuzasjota <chuzas.j@gmail.com>
2019-10-14 12:11:00 -0500

    Tomar datos y agregarlos al objeto -- Languages

--- src/components/Languages.vue
+++ src/components/Languages.vue
@@ -2,7 +2,11 @@
   <div>
     <h1 class="title has-text-centered">Languages</h1>
     <div class="columns is-multiline is-mobile box">
-      <div class="column is-12-mobile is-6-tablet is-6-desktop">
+      <div
+        v-for="(language, index) in formData.languages"
+        v-bind:key="index"
+        class="column is-12-mobile is-6-tablet is-6-desktop"
+      >
         <b-collapse class="card" aria-id="contentIdForA11y3">
           <div
             slot="trigger"
@@ -12,7 +16,7 @@
             aria-controls="contentIdForA11y3"
           >
             <p class="card-header-title">
-              {{ SocialTitle }}
+              {{ language.langTitle }}
             </p>
             <a class="card-header-icon">
               <b-icon
@@ -26,13 +30,22 @@
               <div class="columns is-multiline is-mobile">
                 <div class="column is-12-mobile is-half-tablet is-half-desktop">
                   <b-field label="Language">
-                    <b-input placeholder="English" v-model="SocialTitle">
+                    <b-input
+                      v-model="language.langTitle"
+                      name="langTitle"
+                      placeholder="English"
+                    >
                     </b-input>
                   </b-field>
                 </div>
                 <div class="column is-12-mobile is-half-tablet is-half-desktop">
                   <b-field label="Level">
-                    <b-select placeholder="Select a level" expanded>
+                    <b-select
+                      v-model="language.level"
+                      name="langTitle"
+                      placeholder="Select a level"
+                      expanded
+                    >
                       <option value="native-speaker">Native speaker</option>
                       <option value="highly-proficient">
                         Highly proficient
@@ -43,12 +56,23 @@
                 </div>
               </div>
             </div>
+            <div class="column is-12-mobile is-12-tablet is-12-desktop">
+              <b-button
+                @click="deleteLang(index)"
+                type="is-danger"
+                icon-left="fas fa-trash"
+              >
+                Delete
+              </b-button>
+            </div>
           </div>
         </b-collapse>
       </div>
-      <div class="column is-12-mobile is-12-tablet is-12-desktop">
-        <b-button type="is-danger" icon-left="fas fa-plus">
-          Add new
+      <div
+        class="column is-12-mobile is-12-tablet is-12-desktop has-text-centered"
+      >
+        <b-button @click="addLang" type="is-info" icon-left="fas fa-plus">
+          Add new language
         </b-button>
       </div>
     </div>
@@ -57,6 +81,20 @@
 
 <script>
 export default {
-  name: "Languages"
+  name: "Languages",
+  props: {
+    formData: {}
+  },
+  methods: {
+    addLang: function() {
+      this.formData.languages.push({
+        langTitle: "",
+        level: ""
+      });
+    },
+    deleteLang: function(index) {
+      this.formData.languages.splice(index, 1);
+    }
+  }
 };
 </script>
