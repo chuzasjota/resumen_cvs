c03203c69eed71c32b9ed79393ac03318e2fe0bc
chuzasjota <chuzas.j@gmail.com>
2019-10-14 11:40:33 -0500

    Tomar datos y agregarlos al objeto -- Works

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -2,7 +2,11 @@
   <div>
     <h1 class="title has-text-centered">Works</h1>
     <div class="columns is-multiline is-mobile box">
-      <div class="column is-12-mobile is-6-tablet is-6-desktop">
+      <div
+        v-for="(work, index) in formData.works"
+        v-bind:key="index"
+        class="column is-12-mobile is-6-tablet is-6-desktop"
+      >
         <b-collapse class="card" aria-id="contentIdForA11y3">
           <div
             slot="trigger"
@@ -12,7 +16,7 @@
             aria-controls="contentIdForA11y3"
           >
             <p class="card-header-title">
-              {{ jobTitle }}
+              {{ work.jobTitle }}
             </p>
             <a class="card-header-icon">
               <b-icon
@@ -26,12 +30,22 @@
               <div class="columns is-multiline is-mobile">
                 <div class="column is-12-mobile is-half-tablet is-half-desktop">
                   <b-field label="Job Title">
-                    <b-input placeholder="Web Dev" v-model="jobTitle"></b-input>
+                    <b-input
+                      v-model="work.jobTitle"
+                      name="jobTitle"
+                      placeholder="Web Dev"
+                    >
+                    </b-input>
                   </b-field>
                 </div>
                 <div class="column is-12-mobile is-half-tablet is-half-desktop">
                   <b-field label="Employer">
-                    <b-input placeholder="Multiplica"></b-input>
+                    <b-input
+                      v-model="work.employer"
+                      name="employer"
+                      placeholder="Multiplica"
+                    >
+                    </b-input>
                   </b-field>
                 </div>
                 <div
@@ -39,6 +53,8 @@
                 >
                   <b-field label="Start Date">
                     <b-datepicker
+                      v-model="work.startDate"
+                      name="startDate"
                       placeholder="Click to select..."
                       icon="far fa-calendar-alt"
                     >
@@ -50,6 +66,8 @@
                 >
                   <b-field label="End Date">
                     <b-datepicker
+                      v-model="work.endDate"
+                      name="endDate"
                       placeholder="Click to select..."
                       icon="far fa-calendar-alt"
                     >
@@ -58,22 +76,40 @@
                 </div>
                 <div class="column is-12-mobile is-half-tablet is-half-desktop">
                   <b-field label="City">
-                    <b-input placeholder="CDMX"></b-input>
+                    <b-input v-model="work.city" name="city" placeholder="CDMX">
+                    </b-input>
                   </b-field>
                 </div>
                 <div class="column is-12-mobile is-12-tablet is-12-desktop">
                   <b-field label="Description">
-                    <b-input maxlength="200" type="textarea"></b-input>
+                    <b-input
+                      v-model="work.desc"
+                      name="desc"
+                      maxlength="200"
+                      type="textarea"
+                    >
+                    </b-input>
                   </b-field>
                 </div>
               </div>
             </div>
+            <div class="column is-12-mobile is-12-tablet is-12-desktop">
+              <b-button
+                @click="deleteWork(index)"
+                type="is-danger"
+                icon-left="fas fa-trash"
+              >
+                Delete
+              </b-button>
+            </div>
           </div>
         </b-collapse>
       </div>
-      <div class="column is-12-mobile is-12-tablet is-12-desktop">
-        <b-button type="is-danger" icon-left="fas fa-plus">
-          Add new
+      <div
+        class="column is-12-mobile is-12-tablet is-12-desktop has-text-centered"
+      >
+        <b-button @click="addWork" type="is-info" icon-left="fas fa-plus">
+          Add new work
         </b-button>
       </div>
     </div>
@@ -82,6 +118,24 @@
 
 <script>
 export default {
-  name: "Work"
+  name: "Work",
+  props: {
+    formData: {}
+  },
+  methods: {
+    addWork: function() {
+      this.formData.works.push({
+        jobTitle: "",
+        employer: "",
+        startDate: [],
+        endDate: [],
+        city: "",
+        desc: ""
+      });
+    },
+    deleteWork: function(index) {
+      this.formData.works.splice(index, 1);
+    }
+  }
 };
 </script>
