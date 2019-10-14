91928c31062d8396c13b067c49f6f0864a70e337
chuzasjota <chuzas.j@gmail.com>
2019-10-14 11:55:09 -0500

    Tomar datos y agregarlos al objeto -- Education

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -2,7 +2,11 @@
   <div>
     <h1 class="title has-text-centered">Education</h1>
     <div class="columns is-multiline is-mobile box">
-      <div class="column is-12-mobile is-6-tablet is-6-desktop">
+      <div
+        v-for="(education, index) in formData.educations"
+        v-bind:key="index"
+        class="column is-12-mobile is-6-tablet is-6-desktop"
+      >
         <b-collapse class="card" aria-id="contentIdForA11y3">
           <div
             slot="trigger"
@@ -12,7 +16,7 @@
             aria-controls="contentIdForA11y3"
           >
             <p class="card-header-title">
-              {{ EducationTitle }}
+              {{ education.educationTitle }}
             </p>
             <a class="card-header-icon">
               <b-icon
@@ -26,13 +30,22 @@
               <div class="columns is-multiline is-mobile">
                 <div class="column is-12-mobile is-half-tablet is-half-desktop">
                   <b-field label="School">
-                    <b-input placeholder="Web Dev" v-model="EducationTitle">
+                    <b-input
+                      v-model="education.educationTitle"
+                      name="educationTitlte"
+                      placeholder="Web Dev"
+                    >
                     </b-input>
                   </b-field>
                 </div>
                 <div class="column is-12-mobile is-half-tablet is-half-desktop">
                   <b-field label="Degree">
-                    <b-input placeholder="Multiplica"></b-input>
+                    <b-input
+                      v-model="education.degree"
+                      name="degree"
+                      placeholder="Multiplica"
+                    >
+                    </b-input>
                   </b-field>
                 </div>
                 <div
@@ -40,6 +53,8 @@
                 >
                   <b-field label="Start Date">
                     <b-datepicker
+                      v-model="education.startDate"
+                      name="startDate"
                       placeholder="Click to select..."
                       icon="far fa-calendar-alt"
                     >
@@ -51,6 +66,8 @@
                 >
                   <b-field label="End Date">
                     <b-datepicker
+                      v-model="education.endDate"
+                      name="endDate"
                       placeholder="Click to select..."
                       icon="far fa-calendar-alt"
                     >
@@ -59,22 +76,44 @@
                 </div>
                 <div class="column is-12-mobile is-half-tablet is-half-desktop">
                   <b-field label="City">
-                    <b-input placeholder="CDMX"></b-input>
+                    <b-input
+                      v-model="education.city"
+                      name="city"
+                      placeholder="CDMX"
+                    >
+                    </b-input>
                   </b-field>
                 </div>
                 <div class="column is-12-mobile is-12-tablet is-12-desktop">
                   <b-field label="Description">
-                    <b-input maxlength="200" type="textarea"></b-input>
+                    <b-input
+                      v-model="education.desc"
+                      name="desc"
+                      maxlength="200"
+                      type="textarea"
+                    >
+                    </b-input>
                   </b-field>
                 </div>
               </div>
             </div>
+            <div class="column is-12-mobile is-12-tablet is-12-desktop">
+              <b-button
+                @click="deleteEducation(index)"
+                type="is-danger"
+                icon-left="fas fa-trash"
+              >
+                Delete
+              </b-button>
+            </div>
           </div>
         </b-collapse>
       </div>
-      <div class="column is-12-mobile is-12-tablet is-12-desktop">
-        <b-button type="is-danger" icon-left="fas fa-plus">
-          Add new
+      <div
+        class="column is-12-mobile is-12-tablet is-12-desktop has-text-centered"
+      >
+        <b-button @click="addEducation" type="is-info" icon-left="fas fa-plus">
+          Add new education
         </b-button>
       </div>
     </div>
@@ -83,6 +122,24 @@
 
 <script>
 export default {
-  name: "Education"
+  name: "Education",
+  props: {
+    formData: {}
+  },
+  methods: {
+    addEducation: function() {
+      this.formData.educations.push({
+        educationTitle: "",
+        degree: "",
+        startDate: [],
+        endDate: [],
+        city: "",
+        desc: ""
+      });
+    },
+    deleteEducation: function(index) {
+      this.formData.educations.splice(index, 1);
+    }
+  }
 };
 </script>
