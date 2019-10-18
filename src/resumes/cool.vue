5249d905f72f911eff0d3580079d027d966b693c
chuzasjota <chuzas.j@gmail.com>
2019-10-18 16:14:18 -0500

    Creacion de templates

---
+++ src/resumes/cool.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hi Cool template</h1>
+    <p>{{ formData.basics.name }}</p>
+    <p>{{ formData.basics.email }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cool",
+  props: {
+    formData: {}
+  }
+};
+</script>
---
+++ src/resumes/creative.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="resume">
+    <h1>Hi creative template</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Creative"
+};
+</script>
