c0a8a04b3e819e3f5ba9cad1e37005967d8e6237
chuzasjota <chuzas.j@gmail.com>
2019-10-30 13:27:08 -0600

    Update add-edit Work Form

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -69,9 +69,20 @@
             </b-input>
           </b-field>
         </div>
-        <div class="column is-12-mobile is-12-tablet is-12-desktop">
+        <div
+          v-if="edit == false"
+          class="column is-12-mobile is-12-tablet is-12-desktop"
+        >
           <b-button @click="addWork" type="is-info" icon-left="fas fa-plus">
             Add new work
+          </b-button>
+        </div>
+        <div
+          v-if="edit == true"
+          class="column is-12-mobile is-12-tablet is-12-desktop"
+        >
+          <b-button @click="update()" type="is-info" icon-left="fas fa-plus">
+            Update Work
           </b-button>
         </div>
       </div>
@@ -80,12 +91,13 @@
           <div class="column is-12-mobile is-full-tablet is-full-desktop">
             {{ work.jobTitle }}
             {{ work.employer }}
+            {{ work.city }}
             {{ work.startDate }}
             {{ work.endDate }}
           </div>
           <div class="column is-12-mobile is-6-tablet is-6-desktop">
             <b-button
-              @click="editWork(work)"
+              @click="editWork(index)"
               icon-left="far fa-edit"
               size="is-small"
             >
@@ -108,6 +120,12 @@
 export default {
   name: "Work",
   props: ["formData"],
+  data() {
+    return {
+      edit: false,
+      idWork: 0
+    };
+  },
   methods: {
     addWork() {
       this.formData.works.push({
@@ -125,13 +143,34 @@ export default {
       this.formData.works.endDate = [];
       this.formData.works.desc = "";
     },
-    editWork(work) {
-      this.formData.works.jobTitle = work.jobTitle;
-      this.formData.works.employer = work.employer;
-      this.formData.works.city = work.city;
-      this.formData.works.startDate = work.startDate;
-      this.formData.works.endDate = work.endDate;
-      this.formData.works.desc = work.desc;
+    editWork(index) {
+      this.idWork = index;
+      this.formData.works.jobTitle = this.formData.works[this.idWork].jobTitle;
+      this.formData.works.employer = this.formData.works[this.idWork].employer;
+      this.formData.works.city = this.formData.works[this.idWork].city;
+      this.formData.works.startDate = this.formData.works[
+        this.idWork
+      ].startDate;
+      this.formData.works.endDate = this.formData.works[this.idWork].endDate;
+      this.formData.works.desc = this.formData.works[this.idWork].desc;
+      this.edit = true;
+    },
+    update() {
+      this.formData.works[this.idWork].jobTitle = this.formData.works.jobTitle;
+      this.formData.works[this.idWork].employer = this.formData.works.employer;
+      this.formData.works[this.idWork].city = this.formData.works.city;
+      this.formData.works[
+        this.idWork
+      ].startDate = this.formData.works.startDate;
+      this.formData.works[this.idWork].endDate = this.formData.works.endDate;
+      this.formData.works[this.idWork].desc = this.formData.works.desc;
+      this.formData.works.jobTitle = "";
+      this.formData.works.employer = "";
+      this.formData.works.city = "";
+      this.formData.works.startDate = [];
+      this.formData.works.endDate = [];
+      this.formData.works.desc = "";
+      this.edit = false;
     },
     deleteWork(index) {
       this.formData.works.splice(index, 1);
